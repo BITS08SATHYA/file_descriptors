c10de417d3415dd4cbd603f0431ec67d7095c6b8
Sathya <neelasathya06@gmail.com>
2025-07-06 11:44:31 -0400

    env of a process second use-case

--- fds/source_files/virtual_process/env_of_a_process/env_2.c
+++ fds/source_files/virtual_process/env_of_a_process/env_2.c
@@ -1,3 +1,23 @@
 //
 // Created by sathipa on 6/7/25.
 //
+#include<stdio.h>
+#include<stdlib.h>
+
+void call_func1(void);
+
+int main(){
+    putenv("PARAM1=1024");
+
+    printf("PATH : (%s)\n", getenv("PATH"));
+    printf("HOME : (%s)\n", getenv("HOME"));
+    printf("PARAM1 : (%s)\n", getenv("PARAM1"));
+
+    call_func1();
+
+    return(0);
+}
+
+void call_func1(void){
+    printf("inside function call_func1: PARAM1: (%s)\n", getenv("PARAM1"));
+}
